d2e0798b8bde9a096439b762038db4542fa053e3
Ahmed Elsaadany <ahmed.saadani1011@gmail.com>
2025-05-10 16:45:24 +0300

    last update by ayman for this module at 9/5/2025

--- LTSM/MBINIT/Functional_Lane_Setup.v
+++ LTSM/MBINIT/Functional_Lane_Setup.v
@@ -13,13 +13,27 @@ always @(posedge CLK or negedge rst_n) begin
         done_setup <= 1'b0;
     end else begin
         if (start_setup) begin  
-            if (&i_Transmitter_initiated_Data_to_CLK_Result) 
-                o_Functional_Lanes <= 2'b11;
-            else if (&i_Transmitter_initiated_Data_to_CLK_Result[15:8]) 
-                o_Functional_Lanes <= 2'b10;
-            else if (&i_Transmitter_initiated_Data_to_CLK_Result[7:0]) 
-                o_Functional_Lanes <= 2'b01;
-            done_setup <= 1'b1;
+            if (~done_setup) begin 
+                if (&i_Transmitter_initiated_Data_to_CLK_Result) 
+                    o_Functional_Lanes <= 2'b11;
+                else if (&i_Transmitter_initiated_Data_to_CLK_Result[15:8]) 
+                    o_Functional_Lanes <= 2'b10;
+                else if (&i_Transmitter_initiated_Data_to_CLK_Result[7:0]) 
+                    o_Functional_Lanes <= 2'b01;
+                else 
+                    o_Functional_Lanes <= 2'b00;
+                done_setup <= 1'b1;
+            end else begin
+                done_setup <= 1'b0;
+                if (o_Functional_Lanes == 2'b10) begin
+                    if (&i_Transmitter_initiated_Data_to_CLK_Result[15:8]) o_Functional_Lanes <= 2'b10;
+                    else o_Functional_Lanes <=0;
+                end else if(o_Functional_Lanes == 2'b01) begin
+                    if (&i_Transmitter_initiated_Data_to_CLK_Result[7:0]) o_Functional_Lanes <= 2'b01;
+                    else o_Functional_Lanes <=0;                
+                end
+                done_setup <= 1'b1;
+            end
         end
     end
 end
